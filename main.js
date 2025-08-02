201deaefc53bb9727698db88c617dfba8608c8c5
dfhhgh <amirtamo3@gmail.com>
2025-08-02 23:05:51 +0300

    welcome to js

---
+++ main.js
@@ -0,0 +1,10 @@
+//ctrl + /
+// window.alert("hello");
+// document.writeln("<h1> hello</h1>");
+console.log("hello");
+//styling
+console.log("%cAmigo", "color: red; font-size:100px");
+//don't forget %c before the sentence you need
+//if you want specific words %c word %c
+//************************************************************************** */
+//ECMA
