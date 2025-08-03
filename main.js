60db374f394a7bc67942ecb07f39f10097b75afe
dfhhgh <amirtamo3@gmail.com>
2025-08-03 11:05:50 +0300

    10 to 17

--- main.js
+++ main.js
@@ -8,3 +8,39 @@ console.log("%cAmigo", "color: red; font-size:100px");
 //if you want specific words %c word %c
 //************************************************************************** */
 //ECMA
+// console.log(typeof 500.3);
+// console.log(typeof ["asd","sdsa"]);
+// console.log(typeof {name: "Amigo" , age:14 , country: "EG"});
+// console.log(typeof true);
+// console.log(typeof null);
+// console.log(typeof undefined);
+// var user="Yammmi";
+// var name="amsd",age=21;
+// console.log(Hello);
+//************************************** */
+//redec;are
+//var
+//let
+//const
+// console.log("sad 'dada' \\");
+/************************ */
+//concatenation
+let a="Amigo";
+let b="Man";
+// document.writeln(a + " " +b);
+/********************* */
+//etma
+console.log(`${a} "" '' / \\`);
+//عشان تعملها دوس حرف الذال
+/************* */
+//markup
+let Title="AMigo"
+let markup=`
+<div class='card'>
+
+<div class="child">
+<h2>${Title}<\h2>
+<p>paragraph</p>
+</div>
+</div>`
+document.writeln(markup);
